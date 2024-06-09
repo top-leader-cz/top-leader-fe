66bee6dae8a318562a0ca566de1e83110531afdc
Matej Matiasko <matej.matiasko@pricefx.eu>
2024-06-09 19:04:22 +0700

    Hide export on fetch error

--- src/components/Table/TLTableWithHeader.js
+++ src/components/Table/TLTableWithHeader.js
@@ -155,7 +155,7 @@ export const TLTableWithHeader = ({
           subheader={subheader}
           action={
             <Stack direction="row" spacing={2}>
-              {exportCsv && (
+              {!query.error && exportCsv && (
                 <ButtonMenu
                   button={{
                     variant: "outlined",
